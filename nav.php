4cbec4c1335acf10b7c74caac7d36766c9a7ff5d
dryfish118 <dryfish118@163.com>
2017-12-12 18:02:15 +0800

    new

---
+++ nav.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "<div class='navigate'>";
+echo "<ul class='pc-nav'>";
+echo "<li><a href='./user.php'>人员</a></li>";
+echo "<li><a href='./issue.php'>条目</a></li>";
+echo "<li><a href='./flow.php'>流水</a></li>";
+echo "<li><a href='./brand.php'>品牌</a></li>";
+echo "<li><a href='./cat.php'>类别</a></li>";
+echo "<li><a href='./size.php'>尺寸</a></li>";
+echo "<li><a href='./color.php'>颜色</a></li>";
+echo "<li><a href='./client.php'>客户</a></li>";
+echo "<li><a href='./goods.php'>商品</a></li>";
+echo "<li><a href='./stock.php'>进货</a></li>";
+echo "<li><a href='./sell.php'>销售</a></li>";
+echo "<li><a href='./query_stock.php'>进货</a></li>";
+echo "<li><a href='./query_reserve.php'>库存销售</a></li>";
+echo "<li><a href='./query_income.php'>收支</a></li>";
+echo "<li><a href='./query_sell.php'>销售查询</a></li>";
+echo "<li><a href='./sendip.php'>系统</a></li>";
+echo "<li><a href='./history.php'>历史</a></li>";
+echo "<li><a href='./diary.php'>日志</a></li>";
+echo "<li><a href='./login.php?fout=1'>注销</a></li>";
+echo "</ul>";
+echo "</div>";
+
+?>
+
