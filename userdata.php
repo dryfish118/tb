c16be1bb9a43f0f0b91cd26479e12c1ed4e39581
b <a>
2017-12-25 16:13:50 +0800

    user

---
+++ userdata.php
@@ -0,0 +1,20 @@
+<?php require_once("conn.php") ?>
+<?php
+	$sql = "select * from user";
+    $rs = $conn->query($sql);
+    if ($rs) {
+        $count = 0;
+        $json = "{";
+        while ($row = $rs->fetch_assoc()) {
+            if ($count) {
+                $json .= ",";
+            }
+            $count++;
+            $json .= "{\"id\":" . $row["user_id"] . ",\"name\":" . $row["user_name"] . "}";
+        }
+        $json .= "}";
+        $rs->free();
+
+        echo $json;
+    }
+?>
