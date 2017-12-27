ec4637027e83d4cbae767a240261aa981ce61eb7
b <a>
2017-12-27 08:22:59 +0800

    dxx

--- dxx.js
+++ dxx.js
@@ -1,75 +1,87 @@
-﻿String.prototype.trim = function()
-{
-    return this.replace(/(^\s*)|(\s*$)/g,"");
+﻿String.prototype.trim = function() {
+    return this.replace(/(^\s*)|(\s*$)/g, "");
 }
 
-function onSort(page, col, dir)
-{
+function onSort(page, col, dir) {
     sort.fpage.value = page;
     sort.fcolumn.value = col;
     sort.fdirection.value = dir;
     sort.submit();
 }
 
-function jsToday()
-{
+function jsToday() {
     var dt = new Date();
     return dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
 }
 
-function updateItemToSelect(objSelect, objItemValue)
-{
-    for (var i = 0; i < objSelect.options.length; i++)
-    {
-        if (objSelect.options[i].value == objItemValue)
-        {
+function updateItemToSelect(objSelect, objItemValue) {
+    for (var i = 0; i < objSelect.options.length; i++) {
+        if (objSelect.options[i].value == objItemValue) {
             objSelect.options[i].selected = true;
             break;
         }
     }
 }
 
-function getTextFromSelected(objSelect)
-{
+function getTextFromSelected(objSelect) {
     var i = 0;
-    for (; i < objSelect.options.length; i++)
-    {
-        if (objSelect.options[i].selected)
-        {
+    for (; i < objSelect.options.length; i++) {
+        if (objSelect.options[i].selected) {
             break;
         }
     }
     return objSelect.options[i].text;
 }
 
-function getTextByValue(objSelect, v)
-{
+function getTextByValue(objSelect, v) {
     var i = 0;
-    for (; i < objSelect.options.length; i++)
-    {
-        if (objSelect.options[i].value == v)
-        {
+    for (; i < objSelect.options.length; i++) {
+        if (objSelect.options[i].value == v) {
             break;
         }
     }
     return objSelect.options[i].text;
 }
 
-function getCookie(name)
-{
+function getCookie(name) {
     var start = document.cookie.indexOf(name + '=');
-    if (start == -1)
-    {
+    if (start == -1) {
         return "";
     }
     start += name.length + 1;
     var end = document.cookie.indexOf(';', start);
-    if (end == -1)
-    {
+    if (end == -1) {
         return unescape(document.cookie.substring(start));
-    }
-    else
-    {
+    } else {
         return unescape(document.cookie.substring(start, end));
     }
+}
+
+function loadUser() {
+    document.title = "人员";
+    $.ajax({
+        type: 'GET',
+        url: 'user.php',
+        dataType: 'text',
+        success: function(rawData) {
+            data = $.parseJSON(rawData);
+            user = data.user;
+            var html = "<div id='useradd'><label>人员：</label>" +
+                "<input type='text' name='fname' />" +
+                "<input type='submit' value='增加' />" +
+                "<input type='reset' />" +
+                "</div><div id='userlist'>";
+            $.each(user, function(i, info) {
+                html += "<div class=\'user\'>" +
+                    "<div class=\'id\'>" + info['id'] + "</div>" +
+                    "<div class=\'name\'>" + info['name'] + "</div>" +
+                    "</div>";
+            });
+            html += "</div>";
+            $('#main').html(html);
+        },
+        error: function() {
+            $('#main').html('failed to get user list.');
+        }
+    });
 }
--- user.php
+++ user.php
@@ -1,164 +1,20 @@
-﻿<?php require_once("conn.php"); ?>
+﻿<?php require_once("conn.php") ?>
 <?php
-    $operator = isset($_POST["foperator"]) ? $_POST["foperator"] : OPERATOR_INVALID;
-    $id = isset($_POST["fid"]) ? $_POST["fid"] : 0;
-    $name = isset($_POST["fname"]) ? $_POST["fname"] : "";
+	$sql = "select * from user";
+    $rs = $conn->query($sql);
+    if ($rs) {
+        $count = 0;
+        $json = "{\"user\":[";
+        while ($row = $rs->fetch_assoc()) {
+            if ($count) {
+                $json .= ",";
+            }
+            $count++;
+            $json .= "{\"id\":" . $row["user_id"] . ",\"name\":\"" . $row["user_name"] . "\"}";
+        }
+        $json .= "]}";
+        $rs->free();
 
-    if ($operator == OPERATOR_REMOVE)
-    {
-        if ($id != 0)
-        {
-            $sql = "select user_name from user where user_id = $id";
-            $rs = $conn->query($sql);
-            if ($rs)
-            {
-                if ($row = $rs->fetch_assoc())
-                {
-                    $user_name = $row["user_name"];
-                    $sql = "delete from user where user_id = $id";
-                    if ($conn->query($sql))
-                    {
-                        addHistory("删除", "人员", $user_name);
-                    }
-                    else
-                    {
-                        die($sql . "<br />" . $conn->connect_error . "<br />删除失败");
-                    }
-                }
-                $rs->free();
-            }
-            else
-            {
-                die($sql . "<br />" . $conn->connect_error . "<br />删除失败");
-            }
-        }
-    }
-    else if ($operator == OPERATOR_ADD)
-    {
-        if ($name != "")
-        {
-            $sql = "insert into user(user_name) values('$name')";
-            if ($conn->query($sql))
-            {
-                addHistory("添加", "人员", $name);
-            }
-            else
-            {
-                die($sql . "<br />" . $conn->connect_error . "<br />添加失败");
-            }
-        }
-    }
-    else if ($operator == OPERATOR_MODIFY)
-    {
-        if ($id != 0 && $name != "")
-        {
-            $sql = "select user_name from user where user_id = $id";
-            $rs = $conn->query($sql);
-            if ($rs)
-            {
-                if ($row = $rs->fetch_assoc())
-                {
-                    $user_name = $row["user_name"];
-                    $sql = "update user set user_name = '$name' where user_id = $id";
-                    if ($conn->query($sql))
-                    {
-                        addHistory("修改", "人员", "$name($user_name)");
-                    }
-                    else
-                    {
-                        die($sql . "<br />" . $conn->connect_error . "<br />修改失败");
-                    }
-                }
-                $rs->free();
-            }
-            else
-            {
-                die($sql . "<br />" . $conn->connect_error . "<br />修改失败");
-            }
-        }
+        echo $json;
     }
 ?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="Content-Language" content="zh-cn" />
-<link rel="stylesheet" type="text/css" href="./tb.css" />
-<script type="text/javascript" src="./dxx.js" ></script>
-<script type="text/javascript">
-    function onRemove(id, name)
-    {
-        if (confirm("确定要删除\"" + name + "\"吗？"))
-        {
-            user.foperator.value = <?php echo OPERATOR_REMOVE; ?>;
-            user.fid.value = id;
-            user.submit();
-        }
-    }
-    function onModify(id, name)
-    {
-        user.foperator.value = <?php echo OPERATOR_MODIFY; ?>;
-        user.fid.value = id;
-        user.fname.value = name;
-        user.fsub.value = "修改";
-        
-        scroll(0,0);
-    }
-    function onClear()
-    {
-        user.foperator.value = <?php echo OPERATOR_ADD; ?>;
-        user.fid.value = 0;
-        user.fname.value = "";
-        user.fsub.value = "增加";
-    }
-    function onCheck()
-    {
-        user.fname.value = user.fname.value.trim();
-        if (user.fname.value == "")
-        {
-            alert("输入名字");
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-</script>
-<title>人员</title>
-</head>
-<body>
-<?php
-    require_once("nav.php");
-?>
-    <form method="post" name="user" action="./user.php" onsubmit="return onCheck()" >
-        <label>人员：</label><input type="text" name="fname"/>
-        <input type="hidden" name="foperator" value="<?php echo OPERATOR_ADD; ?>" />
-        <input type="hidden" name="fid" value="0" />
-        <input type="submit" name="fsub" value="增加" />
-        <input type="reset" onclick="onClear()" />
-    </form>
-
-<?php
-        $sql = "select * from user ";
-        $sort_key = array("", "user_name");
-        $rs = $conn->query($sql);
-		if ($rs)
-		{
-			while ($row = $rs->fetch_assoc())
-			{
-                echo "<tr><td>";
-                echo $row["user_name"];
-				// echo "<a href='javascript:void(0)' onclick='onRemove(" 
-				// 	. $row["user_id"] . ", \"" . $row["user_name"] . "\")'>X</a>";
-				// echo "</td><td>";
-				// echo "<a href='javascript:void(0)' onclick='onModify("
-				// 	. $row["user_id"] . ", \"" . $row["user_name"] . "\")'>"
-				// 	. $row["user_name"] . "</a>";
-				echo "</td></tr>";
-            }
-            $rs->free();
-		}
-?>
-</body>
-</html>
