d8a2354e2245503d7fa8ade0f82108a5213ea0ef
dryfish118 <dryfish118@163.com>
2018-01-03 08:37:39 +0800

    color

--- color.php
+++ color.php
@@ -1,184 +1,109 @@
-﻿<?php require_once("colordata.php") ?>
+﻿<?php require_once("conn.php") ?>
 <?php
-    $operator = isset($_POST["foperator"]) ? $_POST["foperator"] : OPERATOR_INVALID;
-    $id = isset($_POST["fid"]) ? $_POST["fid"] : 0;
-    $name = isset($_POST["fname"]) ? $_POST["fname"] : "";
+if (!isset($_POST["fuser"])) {
+    return;
+}
 
-    if ($operator == OPERATOR_REMOVE)
-    {
-        if ($id != 0)
-        {
-            $sql = "select color_name, color_order from color where color_id = $id";
-            $rs = mysql_query($sql);
-            if ($rs && $row = mysql_fetch_array($rs))
-            {
-                $color_name = $row["color_name"];
-                $color_order = $row["color_order"];
-                mysql_query("begin");
-                $sql = "update color set color_order = color_order - 1 where color_order > $color_order";
-                if (!mysql_query($sql))
-                {
-                    mysql_query("rollback");
-                    die($sql . "<br />" . $conn->connect_error . "<br />删除失败");
-                }
-                $sql = "delete from color where color_id = $id";
-                if (mysql_query($sql))
-                {
-                    mysql_query("commit");
-                    addHistory("删除", "颜色", $color_name);
-                }
-                else
-                {
-                    mysql_query("rollback");
-                    die($sql . "<br />" . $conn->connect_error . "<br />删除失败");
-                }
-            }
-            else
-            {
-                die($sql . "<br />" . $conn->connect_error . "<br />删除失败");
-            }
-        }
-    }
-    else if ($operator == OPERATOR_ADD)
-    {
-        if ($name != "")
-        {
-            $sql = "select max(color_order) + 1 as color_max from color";
-            $rs = mysql_query($sql);
-            if ($rs)
-            {
-                $row = mysql_fetch_array($rs);
-				$color_max = $row["color_max"];
-                if ($color_max == "")
-                {
-					$color_max = 1;
-                }
-                $sql = "insert into color(color_name, color_order) values('$name', '$color_max')";
-                if (mysql_query($sql))
-                {
-                    addHistory("添加", "颜色", $name);
-                }
-                else
-                {
-                    die($sql . "<br />" . $conn->connect_error . "<br />添加失败");
-                }
-            }
-            else
-            {
-                die($sql . "<br />" . $conn->connect_error . "<br />添加失败");
-            }
-        }
-    }
-    else if ($operator == OPERATOR_MODIFY)
-    {
-        if ($id != 0 && $name != "")
-        {
-            $sql = "select color_name from color where color_id = $id";
-            $rs = mysql_query($sql);
-            if ($rs && $row = mysql_fetch_array($rs))
-            {
-                $color_name = $row["color_name"];
-                $sql = "update color set color_name = '$name' where color_id = $id";
-                if (mysql_query($sql))
-                {
-                    addHistory("修改", "颜色", "$name($color_name)");
-                }
-                else
-                {
-                    die($sql . "<br />" . $conn->connect_error . "<br />修改失败");
-                }
-            }
-            else
-            {
-                die($sql . "<br />" . $conn->connect_error . "<br />修改失败");
-            }
-        }
-    }
-?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="Content-Language" content="zh-cn" />
-<script type="text/javascript" src="./dxx.js" ></script>
-<script type="text/javascript">
-<?php
-    defineSortFunction("color");
-?>
-    function onRemove(fid, fname)
-    {
-        if (confirm("确定要删除\"" + fname + "\"吗？"))
-        {
-            color.foperator.value = <?php echo OPERATOR_REMOVE; ?>;
-            color.fid.value = fid;
-            color.submit();
-        }
-    }
-    function onModify(fid, fname)
-    {
-        color.foperator.value = <?php echo OPERATOR_MODIFY; ?>;
-        color.fid.value = fid;
-        color.fname.value = fname;
-        color.fsub.value = "修改";
+if (!isset($_POST["faction"])) {
+    return;
+}
 
-        scroll(0, 0);
-    }
-    function onClear()
-    {
-        color.foperator.value = <?php echo OPERATOR_ADD; ?>;
-        color.fid.value = 0;
-        color.fname.value = "";
-        color.fsub.value = "增加";
-    }
-    function onCheck()
-    {
-        color.fname.value = color.fname.value.trim();
-        if (color.fname.value == "")
-        {
-            alert("输入颜色");
-            return false;
+switch ($_POST["faction"]) {
+    case "list" : {
+        $sql = "select * from color order by color_order";
+        $rs = $conn->query($sql);
+        if ($rs) {
+            $count = 0;
+            $json = "{\"color\":[";
+            while ($row = $rs->fetch_assoc()) {
+                if ($count) {
+                    $json .= ",";
+                }
+                $count++;
+                $json .= "{\"id\":" . $row["color_id"] . 
+                    ",\"name\":\"" . $row["color_name"] . 
+                    "\",\"order\":\"" . $row["color_order"] . "\"}";
+            }
+            $json .= "]}";
+            $rs->free();
+    
+            echo $json;
         }
-        else
-        {
-            return true;
-        }
+        break;
     }
-</script>
-<title>颜色</title>
-</head>
-<body>
-<?php
-    defineSort2Form();
+    case "add": {
+        $result = 0;
+        if (isset($_POST["fname"]) && $_POST["fname"] != "") {
+            $sql = "insert into color(color_name, color_order) select '" .
+                $_POST["fname"] . "', (max(color_order) + 1) from color";
+            if ($conn->query($sql)) {
+                $result = addHistory($_POST["fuser"], "add", "color", $_POST["fname"]);
+            }
+        }
+        echo $result;
+        break;
+    }
+    case "delete": {
+        $result = 0;
+        if (isset($_POST["fid"])) {
+            $sql = "select color_name, color_order from color where color_id='" . $_POST["fid"] . "'";
+            $rs = $conn->query($sql);
+            if ($rs) {
+                $row = $rs->fetch_assoc();
+                $fname = $row["color_name"];
+                $forder = $row["color_order"];
+                
+                $conn->autocommit(false);
+                $sql = "delete from color where color_id='" . $_POST["fid"] . "'";
+                if ($conn->query($sql)) {
+                    $sql = "update color set color_order = color_order - 1 where color_order > " . $color_order;
+                    if ($conn->query($sql)) {
+                        $result = addHistory($_POST["fuser"], "delete", "color", $fname);
+                        $conn->commit();
+                    } else {
+                        $conn->rollback();
+                    }
+                } else {
+                    $conn->rollback();
+                }
+            }
+        }
+        echo $result;
+        break;
+    }
+    case "update": {
+        $result = 0;
+        if (isset($_POST["fid"]) && isset($_POST["fname"]) && $_POST["fname"] != "") {
+            $sql = "select color_name from color where color_id='" . $_POST["fid"] . "'";
+            $rs = $conn->query($sql);
+            if ($rs) {
+                $row = $rs->fetch_assoc();
+                $fname = $row["color_name"];
+                $sql = "update color set color_name='" . $_POST["fname"] .
+                    "' where color_id='" . $_POST["fid"] ."'";
+                if ($conn->query($sql)) {
+                    $result = addHistory($_POST["fuser"], "update", "color", $_POST["fname"] . "(" . $fname . ")");
+                }
+            }
+        }
+        echo $result;
+        break;
+    }
+    case "top": {
+        echo toTop("color");
+        break;
+    }
+    case "bottom": {
+        echo toBottom("color");
+        break;
+    }
+    case "up": {
+        echo toUp("color");
+        break;
+    }
+    case "down": {
+        echo toDown("color");
+        break;
+    }
+}
 ?>
-    <form method="post" name="color" action="./color.php" onsubmit="return onCheck()" >
-        <lable>颜色：</lable><input type="text" name="fname" />
-        <input type="hidden" name="foperator" value="<?php echo OPERATOR_ADD; ?>" />
-        <input type="hidden" name="fid" value="0" />
-        <input type="submit" name="fsub" value="增加" />
-        <input type="reset" onclick="onClear()" />
-    </form>
-    <table frame="border" rules="all">
-        <tr>
-            <th width="10"></th>
-            <th width="300">颜色</th>
-            <th width="100">排序</th>
-        </tr>
-    <?php
-        $cs = new CColorSet;
-        foreach ($cs->m_items as $v)
-        {
-            echo "        <tr>\n            <td>\n                ";
-            echo "<a href='javascript:void(0)' onclick='onRemove("
-                  . $v->id. ", \"" . $v->name . "\")'>X</a>";
-            echo "\n            </td>\n            <td>\n                ";
-            echo "<a href='javascript:void(0)' onclick='onModify("
-                  . $v->id . ", \"" . $v->name . "\")'>"
-                  . $v->name . "</a>";
-            echo "\n            </td>\n            <td align = 'center'>\n                ";
-            generalOrderHtml($v, $cs->m_items);
-            echo "\n            </td>\n        </tr>\n";
-        }
-    ?>
-    </table>
-</body>
-</html>
--- dxx.js
+++ dxx.js
@@ -396,6 +396,122 @@ function loadSize() {
 }
 
 ///////////////////////////////////////
+// color
+function onOrderColor(row, ftype) {
+    var fuser = $.cookie("cookie_user");
+    var $tr = $("#edittable tr").eq(row + 1);
+    var fid = $tr.attr("value");
+    $("#fid").attr("value", fid);
+    $.ajax({
+        type: "POST",
+        url: "./color.php",
+        cache: false,
+        data: {
+            "fuser": fuser,
+            "faction": ftype,
+            "fid": fid
+        },
+        dataType: "text",
+        success: function(data, textStatus) {
+            if (parseInt(data) == 1) {
+                loadColor();
+            }
+        }
+    });
+}
+
+function onModColor(row) {
+    var $tr = $("#edittable tr").eq(row + 1);
+    var fid = $tr.attr("value");
+    var fname = $tr.children("td").eq(0).text();
+    $("#faction").attr("value", "update");
+    $("#fid").attr("value", fid);
+    $("#fname").val(fname);
+}
+
+function onDelColor(row) {
+    if (!confirm("确定要删除吗？")) {
+        return;
+    }
+    var fuser = $.cookie("cookie_user");
+    var $tr = $("#edittable tr").eq(row + 1);
+    var fid = $tr.attr("value");
+    $.ajax({
+        type: "POST",
+        url: "./color.php",
+        cache: false,
+        data: {
+            "fuser": fuser,
+            "faction": "delete",
+            "fid": fid
+        },
+        dataType: "text",
+        success: function(data, textStatus) {
+            if (parseInt(data) == 1) {
+                loadColor();
+            }
+        }
+    });
+}
+
+function onColor() {
+    var fuser = $.cookie("cookie_user");
+    var faction = $("#faction").attr("value");
+    var fid = $("#fid").attr("value");
+    var fname = $("#fname").val();
+    $("#faction").attr("value", "add");
+    $.ajax({
+        type: "POST",
+        url: "./color.php",
+        cache: false,
+        data: {
+            "fuser": fuser,
+            "faction": faction,
+            "fid": fid,
+            "fname": fname
+        },
+        dataType: "text",
+        success: function(data, textStatus) {
+            if (parseInt(data) == 1) {
+                loadColor();
+                return true;
+            }
+        }
+    });
+
+    return false;
+}
+
+function loadColor() {
+    document.title = "颜色";
+    $.post("color.php", {
+        "fuser": $.cookie("cookie_user"),
+        "faction": "list"
+    }, function(rawData, textStatus) {
+        var data = $.parseJSON(rawData);
+        var color = data.color;
+
+        var st = new SmartTable();
+        st.setOrder("onOrderColor");
+        st.setModify("onModColor");
+        st.setDelete("onDelColor");
+        st.setHeader(["颜色"]);
+        $.each(color, function(i, item) {
+            st.addRow(item.id, [item.name]);
+        });
+
+        var html = "<form id='editform' onsubmit='return onColor();'>" +
+            "<input type='hidden' id='faction' value='add' />" +
+            "<input type='hidden' id='fid' value='0' />" +
+            "<label>颜色</label><input type='text' id='fname' />" +
+            "<input type='submit' /><input type='reset' />" +
+            "</form>" +
+            "<div>" + st.getTable() + "</div>";
+        $('#main').html(html);
+    });
+}
+
+///////////////////////////////////////
 // history
 function loadHistory() {
     document.title = "历史";
@@ -439,6 +555,11 @@ function loadMain(page) {
         case "size":
             {
                 loadSize();
+                break;
+            }
+        case "color":
+            {
+                loadColor();
                 break;
             }
         case "history":
